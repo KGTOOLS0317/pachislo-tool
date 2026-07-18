aeb4cf78e178c6faec6edcac2d3d6279c2bc8ff7
KGTOOLS0317 <KGTOOLS0317@users.noreply.github.com>
2026-07-18 07:04:50 +0000

    deploy: 6014939d3a61dda44a6c0ad0979486337be1cc93

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'pachislot-tool-cache-20260718065426'; // Cache version incremented
+const CACHE_NAME = 'pachislot-tool-cache-20260718070447'; // Cache version incremented
 const URLS_TO_CACHE = [
   './',
   './index.html', 
